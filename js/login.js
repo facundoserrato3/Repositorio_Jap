e4a3bf02c6898b8646c462419f34f8daa838d43e
facundoserrato3 <88204052+facundoserrato3@users.noreply.github.com>
2021-08-13 11:58:47 -0300

    4

--- js/login.js
+++ js/login.js
@@ -1,3 +1,12 @@
+var contenedor_formulario = document.getElementsByClassName("contenedor-formulario");
+var formulario_login = document.getElementsByClassName("formulario-login");
+var formulario_registro = document.getElementsByClassName("formulario-registro");
+
+
+// Funcion que mueve cartel de registro
+function registro()
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
