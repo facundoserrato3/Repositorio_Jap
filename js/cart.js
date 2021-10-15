3ea77128f4555bfd21efbee8526267b5df268e36
facundoserrato3 <88204052+facundoserrato3@users.noreply.github.com>
2021-10-14 23:48:01 -0300

    q

--- js/cart.js
+++ js/cart.js
@@ -14,29 +14,39 @@ function borrar(){
    
 
 }
-function subtotal() {
+function calcularSubtotal() {
     let cantidad= document.getElementsByTagName("input");
     let precio = document.getElementsByClassName("precio");
     let subt = document.getElementsByClassName("subt");
     let subtotal = 0
     let total = 0
     let subt2 = 0
+    let totalproductos = 0
     for (let i=0; i<precio.length; i++){
         let precios = precio[i];
         let cantidades= cantidad[i]
-       
         let subts = subt[i]
         subtotal += parseInt(precios.innerHTML) * parseInt(cantidades.value);
         subt2 = parseInt(precios.innerHTML) * parseInt(cantidades.value);
         
         
        subts.innerHTML = "$" + " " + subt2;
-        
+        totalproductos += parseInt(cantidades.value)
     }
     total += parseInt(subtotal);
-    document.getElementById("carrosubtotal").innerHTML = "$" + " " + subtotal;
-    document.getElementById("carrototal").innerHTML = "$" + " " + total;
+    document.getElementById("subtotal-modal").innerHTML = subtotal;
+    document.getElementById("carrosubtotal").innerHTML = subtotal;
+    document.getElementById("carrototal").innerHTML = total;
+    document.getElementById("badge").innerHTML = totalproductos;
         
+
+}
+function tipoDeEnvio(porcentaje) {
+    let subtotalenvio = document.getElementById("carrosubtotal");
+    let porcentajeEnvio = (subtotalenvio.innerHTML * porcentaje) / 100;
+    let preciofinal = parseInt(subtotalenvio.innerHTML) + parseInt(porcentajeEnvio);
+    
+    document.getElementById("carrototal").innerHTML = preciofinal; 
 
 }
 
@@ -47,7 +57,7 @@ function mostrarCarrito(array){
     let articulos = array.articles;
     let total=0;
     let subtotal= 0;
-    let totalproductos = 0;
+    let totalproductos = 0
     
     for (let i=0; i<articulos.length; i++){
         let carrito= articulos[i];
@@ -60,7 +70,7 @@ function mostrarCarrito(array){
         `<tr class="prod">
         <td ><img src="${carrito.src}" width="100" </td>
       <td>${carrito.name}</td>
-    <td><input class="form-control cant" type="number" min="1" id="cant${i}" value="${carrito.count}" onchange="subtotal()" ></td>
+    <td><input class="form-control cant" type="number" min="1" id="cant${i}" value="${carrito.count}" onchange="calcularSubtotal()" ></td>
     
     <td >$ <span class="precio">${carrito.unitCost}</span></td> 
     <td class="subt">$ ${carrito.count * carrito.unitCost}</td>
@@ -75,7 +85,7 @@ function mostrarCarrito(array){
             `<tr class="prod">
             <td ><img src="${carrito.src}" width="100"></td>
           <td>${carrito.name}</td>
-        <td><input class="form-control cant" type="number" value="${carrito.count}" id="cant${i}" onchange="subtotal()" min="1"></td>
+        <td><input class="form-control cant" type="number" value="${carrito.count}" id="cant${i}" onchange="calcularSubtotal()" min="1"></td>
         <td >$ <span class="precio">${carrito.unitCost}</span></td> 
         <td class="subt">$ ${carrito.count * carrito.unitCost}</td>
      
@@ -88,18 +98,19 @@ function mostrarCarrito(array){
         
         subtotal += carrito.count *carrito.unitCost;
          
-  totalproductos += parseInt(carrito.count)
+  
     
-        
+        totalproductos += parseInt(carrito.count);
        
 
     }
-    document.getElementById("badge").innerHTML = totalproductos;
+    
     total += parseInt(subtotal);
     document.getElementById("carro").innerHTML = contenidoparaadjuntar;
-    document.getElementById("carrototal").innerHTML = "$" + " " + total;
-    document.getElementById("carrosubtotal").innerHTML = "$" + " " + subtotal;
-    
+    document.getElementById("carrototal").innerHTML = total;
+    document.getElementById("carrosubtotal").innerHTML = subtotal;
+    document.getElementById("badge").innerHTML = totalproductos;
+    document.getElementById("subtotal-modal").innerHTML = subtotal;
 };
 
 
