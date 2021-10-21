cd3b952a5c0ca265e1358282b2c55281a420b4f6
facundoserrato3 <88204052+facundoserrato3@users.noreply.github.com>
2021-10-21 17:43:32 -0300

    h

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,3 +1,29 @@
+function mostrarDatos(){
+    let nombre = document.getElementById("nombres");
+    let apellido = document.getElementById("apellidos");
+    let edad = document.getElementById("edad");
+    let telefono = document.getElementById("telefono");
+    let usuario = JSON.parse(localStorage.datosperfil)
+    nombre.innerHTML = `${usuario.nombres}`;
+    apellido.innerHTML = `${usuario.apellidos}`;
+    edad.innerHTML = `${usuario.edad}`;
+    telefono.innerHTML = `${usuario.telefono}`;
+
+}
+
+function guardarDatosPerfil() {
+    let nuevoNombre = document.getElementById("nuevonombre");
+    let nuevoApellido = document.getElementById("nuevoapellido");
+    let nuevaEdad = document.getElementById("nuevaedad");
+    let nuevoTelefono = document.getElementById("nuevotelefono");
+    localStorage.setItem("datosperfil", JSON.stringify({nombres: nuevoNombre.value, apellidos: nuevoApellido.value, edad: nuevaEdad.value, telefono: nuevoTelefono.value}));
+
+mostrarDatos();
+}
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
