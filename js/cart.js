fa1cbebd520465996ca4542076f3d8c3dfa33d96
facundoserrato3 <88204052+facundoserrato3@users.noreply.github.com>
2021-11-11 22:01:56 -0300

    1

--- js/cart.js
+++ js/cart.js
@@ -47,6 +47,7 @@ function tipoDeEnvio(porcentaje) {
     let preciofinal = parseInt(subtotalenvio.innerHTML) + parseInt(porcentajeEnvio);
     
     document.getElementById("carrototal").innerHTML = preciofinal; 
+    document.getElementById("porcentaje-envio").innerHTML = parseInt(porcentajeEnvio);
 
 }
 
@@ -112,7 +113,17 @@ function mostrarCarrito(array){
     document.getElementById("badge").innerHTML = totalproductos;
     document.getElementById("subtotal-modal").innerHTML = subtotal;
 };
-
+function validate(){
+    let name= document.getElementById("username").value;
+    let creditcard = document.getElementById("creditcard-number").value;
+    let creditcardcode = document.getElementById("creditcard-code").value;
+    let creditcardexpire = document.getElementById("creditcard-expire").value;
+    let bankaccount = document.getElementById("account").value;
+    if(name.trim() !== "" && creditcard.trim() !== "" && creditcardcode.trim() !== "" && creditcardexpire.trim() !=="" || bankaccount.trim() != "") {
+        Swal.fire("La compra ha sido exitosa.");
+    }
+   };
+   
 
 
 
@@ -128,3 +139,51 @@ document.addEventListener("DOMContentLoaded", function(e){
 })
 
 });
+
+document.getElementById("pay-validation").addEventListener("click", function (e){
+    let inputs = document.getElementsByClassName("form-control");
+    for(let i=0; i<inputs.length;i++){
+        let input=inputs[i];
+       if (input.value.trim() === ""){
+           
+       input.classList.add("is-invalid");
+   }
+   else{
+       input.classList.remove("is-invalid");
+       input.classList.add("is-valid");
+   
+   }}
+   validate();
+   
+   });
+   
+   document.getElementById("creditcard").addEventListener("click", function (e){
+       let creditform = document.getElementById("credit-card-form");
+       let clase = creditform.getAttribute("class");
+       let bankform= document.getElementById("bank-form");
+       if(clase === "form-display-none needs-validation" || clase ==="needs-validation form-display-none"){
+       creditform.classList.remove("form-display-none");
+       bankform.classList.remove("form-display-block")
+       bankform.classList.add("form-display-none")
+       creditform.classList.add("form-display-block");
+   } else {
+       creditform.classList.remove("form-display-block");
+       creditform.classList.add("form-display-none");
+   }
+       
+   });
+   document.getElementById("bankdeposit").addEventListener("click", function (e){
+       let creditform = document.getElementById("credit-card-form");
+       let bankform= document.getElementById("bank-form");
+   let clase=bankform.getAttribute("class");
+       if(clase === "form-display-none needs-validation" || clase ==="needs-validation form-display-none"){
+       creditform.classList.remove("form-display-block");
+       creditform.classList.add("form-display-none");
+       bankform.classList.remove("form-display-none");
+       bankform.classList.add("form-display-block");
+   } else {
+       bankform.classList.remove("form-display-block");
+       bankform.classList.add("form-display-none")
+   }
+       
+   });
